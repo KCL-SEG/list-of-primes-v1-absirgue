2b96cfc2dc17bee92c3a59dcf244db1e92fe09df
absirgue <anton.sirgue@kcl.ac.uk>
2022-09-26 18:36:47 +0100

    Added main exercise logic
    Added iteration and selection statements so that the list fills up with the right number of prime numbers.

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    tested_number = 1
+    while len(list) < number_of_primes:
+        numberCanBePrime = True
+        tested_number += 1
+        for i in range(2, tested_number):
+            if (tested_number % i == 0):
+                numberCanBePrime = False
+        if (numberCanBePrime):
+            list.append(tested_number)
     return list
